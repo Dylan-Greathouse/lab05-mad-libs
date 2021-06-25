c5b4925a752584ba293b1fa5b2f51e00eab434d1
Dylan-Greathouse <Greathouse.dylanj@gmail.com>
2021-06-25 14:26:49 -0700

    added html and a bit of javascript or id elements

---
+++ app.js
@@ -0,0 +1,22 @@
+// inputs
+const foodInput = document.getElementById('food-input');
+const adjectiveInput = document.getElementById('adjective-input');
+const bodyPartInput = document.getElementById('body-part-input');
+const senseInput = document.getElementById('sense-input');
+const verbInput = document.getElementById('verb-input');
+const timeOfDayInput = document.getElementById('time-of-day-input');
+const btn = document.getElementById('btn')
+
+//madlibs
+const food = document.getElementById('food');
+const adjective = document.getElementById('adjective');
+const bodyPart = document.getElementById('body-part');
+const sense = document.getElementById('sense');
+const verb = document.getElementById('verb');
+const timeOfDay = document.getElementById('time-of-day');
+
+
+//event listeners
+btn.addEventListener('click', () =>{
+    console.log('you click a button')
+})
